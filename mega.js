ba1307091bd1354498842e12ee6614c379c67f34
vishwamihi <98463616+vishwamihi@users.noreply.github.com>
2024-08-24 20:23:03 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: 'ledav46245@brinkc.com',
-    password: 'ledav46245@brinkc.com',
+    email: 'gaped41762@daypey.com',
+    password: 'gaped41762@daypey.com',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
